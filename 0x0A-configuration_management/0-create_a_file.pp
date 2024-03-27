c9c525ea4a33db6cb621edc1081ae21dfc90f35a
nooby6/Audrine Whrite <giftaudrine1@gmail.com>
2024-03-27 09:13:09 -0700

    Using Puppet, create a file in /tmp.

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# Create a file in /tmp
+file { '/tmp/school':
+  ensure  => 'file',          # Ensure it's a file
+  mode    => '0744',          # Set file permission to 0744
+  owner   => 'www-data',      # Set file owner to www-data
+  group   => 'www-data',      # Set file group to www-data
+  content => 'I love Puppet', # Set file content
+}
