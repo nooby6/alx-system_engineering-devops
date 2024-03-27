91114ea730a7c6be399a6ea537050455939b7db7
nooby6/Audrine Whrite <giftaudrine1@gmail.com>
2024-03-27 09:42:13 -0700

    Using Puppet, install flask from pip3.

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,13 @@
-# Install Flask version 2.1.0 using pip3
+# Install Flask version 2.1.0 and compatible version of Werkzeug using pip3
 package { 'python3-pip':
   ensure => 'installed',
+}
+
+exec { 'install_werkzeug':
+  command     => '/usr/bin/pip3 install Werkzeug==2.0.3',
+  path        => ['/usr/local/bin', '/usr/bin'],
+  environment => ['PATH=/usr/local/bin:/usr/bin'],
+  unless      => '/usr/bin/pip3 show Werkzeug | grep -q "Version: 2.0.3"',
 }
 
 exec { 'install_flask':
