d535621a8ed3fcfb33a0a85bc556de21ea38c3fa
Audrine Whrite <giftaudrine1@gmail.com>
2024-06-06 06:22:59 -0700

    a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,53 +1,24 @@
 #!/usr/bin/python3
 """
-Reddit APIs
-
-This module contains a function that queries the Reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit.
+ Reddit APIs
 """
-
-import requests
+from requests import get
 
 
 def top_ten(subreddit):
+    """queries the Reddit API and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
     """
-    Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {
+        'User-Agent': 'My User Agent 2'
+    }
+    response = get(url, headers=headers, allow_redirects=False)
+    reddits = response.json()
 
-    Parameters:
-    subreddit (str): The name of the subreddit to query.
-
-    If not a valid subreddit, print None.
-    """
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 2'}
-    
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            print(None)
-            return
-        
-        data = response.json().get('data', {})
-        children = data.get('children', [])
-        if not children:
-            print(None)
-            return
-
-        for child in children:
-            title = child.get('data', {}).get('title')
-            if title:
-                print(title)
-            else:
-                print(None)
-
-    except requests.RequestException:
-        print(None)
-
-
-if __name__ == "__main__":
-    import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+        children = reddits.get('data').get('children')
+        for i in range(10):
+            print(children[i].get('data').get('title'))
+    except Exception:
+        print('None')
