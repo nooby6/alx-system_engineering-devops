42bf1724f31a28822c881296fc777035b2194bd5
Audrine Whrite <giftaudrine1@gmail.com>
2024-06-06 06:12:26 -0700

    a function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-"""
-Reddit API Query Module
-
-This module contains a function to query the Reddit API and retrieve the number
-of subscribers for a given subreddit. It handles invalid subreddits by returning 0.
-"""
+"""function that queries the Reddit API and returns the
+number of subscribers for a given subreddit"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given subreddit.
-
-    Parameters:
-    subreddit (str): The name of the subreddit to query.
-
-    Returns:
-    int: The number of subscribers, or 0 if the subreddit is invalid or an error occurs.
-    """
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    """function that queries the Reddit API and returns the number of
+    subscribers for a given subreddit"""
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {'User-Agent': 'My User Agent 1'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except requests.RequestException:
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data').get('subscribers')
+    else:
         return 0
-if __name__ == "__main__":
-    print(number_of_subscribers('learnpython'))  # Example valid subreddit
-    print(number_of_subscribers('thissubredditdoesnotexist123'))  # Example invalid subreddit
