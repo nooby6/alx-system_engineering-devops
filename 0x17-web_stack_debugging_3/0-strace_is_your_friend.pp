3ec93179f098010d0ca5dfc6b717dfb5c899acda
Audrine Whrite <giftaudrine1@gmail.com>
2024-08-13 09:47:39 -0700

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,7 @@
-# 0-strace_is_your_friend.pp
-# This Puppet manifest installs the missing php5-mysql module and restarts Apache
+# Puppet script to fix typo errors in PHP files
 
-exec { 'install-php-mysql':
-  command => '/usr/bin/apt-get install -y php5-mysql',
-  path    => ['/bin', '/usr/bin'],
-}
-
-service { 'apache2':
-  ensure    => 'running',
-  enable    => true,
-  require   => Exec['install-php-mysql'],
-  subscribe => Exec['install-php-mysql'],
+exec { 'fix-php-typo':
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  path    => ['/usr/local/sbin', '/usr/local/bin', '/usr/sbin', '/usr/bin', '/sbin', '/bin'],
+  onlyif  => "grep -q '.phpp' /var/www/html/wp-settings.php",
 }
